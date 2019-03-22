856ef104ed70b850b02a53f4a91359d417089882
bryewalks <bryewalks@gmail.com>
2019-03-21 23:48:51 -0500

    added address to all locations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,20 +1,23 @@
 class Location < ApplicationRecord
   has_many :weathers
-  has_many :user_locations
+  has_many :user_locations, dependent: :destroy
   has_many :users, through: :user_locations
-  has_many :location_reviews, through: :user_locations
+  has_many :location_reviews, dependent: :destroy, through: :user_locations
   enum flight_zone_status: {no_flight_zone: 0, flight_zone: 1, requires_authorization: 2}
   validates :name, presence: true
   validates :address, presence: true
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
+  # reverse_geocoded_by :latitude, :longitude
+  #   # :address => :location
+  # after_validation :reverse_geocode
 
   def check_airports
-    self.nearbys(5).where(airport: true)
+      self.nearbys(5).where(airport: true)
   end
 
   def determine_status
-    if check_airports.length > 0
+    if self.latitude && self.longitude && check_airports.length > 0
       self.flight_zone_status = "no_flight_zone"
     else
       self.flight_zone_status = "flight_zone"
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,15 @@ airports = JSON.parse(airport_file)
 airports_array = []
 
 airports.each do |airport|
-  if !airport["type"]["seaplane_base"] && !airport["type"]["closed"] && !airport["type"]["heliport"] && !airport["type"]["large_airport"] && airport["iso_country"]["US"] && airport["iso_region"]["US-IL"]
+  if !airport["type"]["seaplane_base"] && !airport["type"]["closed"] && !airport["type"]["heliport"] && !airport["type"]["large_airport"] && airport["iso_country"]["US"]
     airports_array << airport
+    # @location = Location.create!(
+    #                   name: airport["name"],
+    #                   latitude: airport["latitude_deg"],
+    #                   longitude: airport["longitude_deg"],
+    #                   flight_zone_status: 0,
+    #                   airport: true
+    #                   )
   end
 end
 
